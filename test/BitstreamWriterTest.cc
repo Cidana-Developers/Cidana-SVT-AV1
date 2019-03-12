f4a8d4cb23a7a23226b375397b51e7ed89a20da1
yao <wyao.liu@gmail.com>
2019-03-12 12:58:43 +0800

    Add TestRandomBits
    1. Use C++11 random generator
    2. Use different pattern for probability and bit

--- test/BitstreamWriterTest.cc
+++ test/BitstreamWriterTest.cc
@@ -15,6 +15,7 @@
  */
 #include <stdlib.h>
 #include <math.h>
+#include <random>
 #include "gtest/gtest.h"
 #include "BitstreamReaderMock.h"
 #include "acm_random.h"
@@ -25,62 +26,85 @@ namespace {
 const int num_tests = 10;
 }
 
-TEST(BitstreamWriter, TestBitIO) {
-    ACMRandom rnd(ACMRandom::DeterministicSeed());
+TEST(BitstreamWriter, TestRandomBits) {
     for (int n = 0; n < num_tests; ++n) {
         // we generate various proba
-        for (int method = 0; method <= 7; ++method) {
-            const int kBitsToTest = 1000;
-            uint8_t probas[kBitsToTest];
+        for (int prob_gen_method = 0; prob_gen_method <= 6; ++prob_gen_method) {
+            const int total_bits = 1000;
+            uint8_t probas[total_bits];
+            const int seed = 0xa42b;  // deterministic seeds
+            std::mt19937 gen(seed);
+            std::uniform_int_distribution<> prob_uni_dist(0, 255);
+            std::uniform_int_distribution<> lowprob_uni_dist(0, 32);
+            std::bernoulli_distribution flip_dist(0.5);
 
-            for (int i = 0; i < kBitsToTest; ++i) {
-                const int parity = i & 1;
-                /* clang-format off */
-                probas[i] =
-                  (method == 0) ? 0 : (method == 1) ? 255 :
-                  (method == 2) ? 128 :
-                  (method == 3) ? rnd.Rand8() :
-                  (method == 4) ? (parity ? 0 : 255) :
-                  // alternate between low and high proba:
-                  (method == 5) ? (parity ? rnd(128) : 255 - rnd(128)) :
-                  (method == 6) ?
-                  (parity ? rnd(64) : 255 - rnd(64)) :
-                  (parity ? rnd(32) : 255 - rnd(32));
-                /* clang-format on */
+            switch (prob_gen_method) {
+                // extreme probas
+            case 0: memset(probas, 0, sizeof(probas)); break;
+            case 1:
+                for (int i = 0; i < total_bits; ++i)
+                    probas[i] = 255;
+                break;
+                //
+            case 2:
+                for (int i = 0; i < total_bits; ++i)
+                    probas[i] = 128;
+                break;
+            case 3:
+                for (int i = 0; i < total_bits; ++i)
+                    probas[i] = prob_uni_dist(gen);
+                break;
+            case 4:
+                for (int i = 0; i < total_bits; ++i)
+                    probas[i] = lowprob_uni_dist(gen);
+                break;
+            case 5:
+                for (int i = 0; i < total_bits; ++i)
+                    probas[i] = 255 - lowprob_uni_dist(gen);
+                break;
+            default:
+                for (int i = 0; i < total_bits; ++i) {
+                    bool flip = flip_dist(gen);
+                    probas[i] = flip ? lowprob_uni_dist(gen)
+                                     : 255 - lowprob_uni_dist(gen);
+                }
+                break;
             }
-            for (int bit_method = 0; bit_method <= 3; ++bit_method) {
-                const int random_seed = 6432;
-                const int kBufferSize = 10000;
-                ACMRandom bit_rnd(random_seed);
-                aom_writer bw;
-                uint8_t bw_buffer[kBufferSize];
-                aom_start_encode(&bw, bw_buffer);
 
-                int bit = (bit_method == 0) ? 0 : (bit_method == 1) ? 1 : 0;
-                for (int i = 0; i < kBitsToTest; ++i) {
-                    if (bit_method == 2) {
-                        bit = (i & 1);
-                    } else if (bit_method == 3) {
-                        bit = bit_rnd(2);
-                    }
-                    aom_write(&bw, bit, static_cast<int>(probas[i]));
+            for (int bit_gen_method = 0; bit_gen_method < 3; ++bit_gen_method) {
+                const int buffer_size = 10000;
+                aom_writer bw;
+                uint8_t bw_buffer[buffer_size];
+                std::bernoulli_distribution bit_dist(0.5);
+                uint8_t test_bits[total_bits];
+
+                // setup test bits
+                switch (bit_gen_method) {
+                case 0: memset(test_bits, 0, sizeof(test_bits)); break;
+                case 1:
+                    for (int i = 0; i < total_bits; ++i)
+                        test_bits[i] = 1;
+                default:
+                    for (int i = 0; i < total_bits; ++i)
+                        test_bits[i] = bit_dist(gen);
                 }
 
+                // encode the bits
+                aom_start_encode(&bw, bw_buffer);
+                for (int i = 0; i < total_bits; ++i) {
+                    aom_write(&bw, test_bits[i], static_cast<int>(probas[i]));
+                }
                 aom_stop_encode(&bw);
 
+                // read out the bits and verify
                 aom_reader br;
                 aom_reader_init(&br, bw_buffer, bw.pos);
-                bit_rnd.Reset(random_seed);
-                for (int i = 0; i < kBitsToTest; ++i) {
-                    if (bit_method == 2) {
-                        bit = (i & 1);
-                    } else if (bit_method == 3) {
-                        bit = bit_rnd(2);
-                    }
-                    GTEST_ASSERT_EQ(aom_read(&br, probas[i], NULL), bit)
-                        << "pos: " << i << " / " << kBitsToTest
-                        << " bit_method: " << bit_method
-                        << " method: " << method;
+                for (int i = 0; i < total_bits; ++i) {
+                    GTEST_ASSERT_EQ(aom_read(&br, probas[i], NULL),
+                                    test_bits[i])
+                        << "pos: " << i << " / " << total_bits
+                        << " bit_gen_method: " << bit_gen_method
+                        << " prob_gen_method: " << prob_gen_method;
                 }
             }
         }
