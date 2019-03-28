845e7f83f321d5a53e7d8312436af8a705589d73
Ryan.Zhao <suanli@gmail.com>
2019-03-28 15:42:37 +0800

    Add width&height padding to support more input source. Add YUV422 YUV444 format support for Y4M file.

--- test/e2e_test/Y4mVideoSource.cc
+++ test/e2e_test/Y4mVideoSource.cc
@@ -104,12 +104,18 @@ EbErrorType Y4MVideoSource::parse_file_info() {
         case 'W':  // Width
         {
             fscanf(file_handle_, "%d ", &width_);
+            width_with_padding_ = width_;
+            if (width_ % 8 != 0)
+                width_with_padding_ += +(8 - width_ % 8);
             fseek(file_handle_, -1, SEEK_CUR);
         } break;
         case 'H':  // Height
         {
             fscanf(file_handle_, "%d ", &height_);
             fseek(file_handle_, -1, SEEK_CUR);
+            height_with_padding_ = height_;
+            if (height_ % 8 != 0)
+                height_with_padding_ += (8 - height_ % 8);
         } break;
         case 'F':  // Frame rate
         {
@@ -131,11 +137,36 @@ EbErrorType Y4MVideoSource::parse_file_info() {
         } break;
         case 'C':  // Color space
         {
-            SKIP_TAG;
-            // TODO:Support other type, support 420 currently.
-            image_format_ = IMG_FMT_420;
+            char line[80] = {0};
+            int line_len = 0;
+            char c;
+            do {
+                line[line_len++] = c = fgetc(file_handle_);
+            } while (c != ' ' && c != 0x0A && line_len < 80);
+            if (strncmp("420jpeg", line, 7) == 0 ||
+                strncmp("420", line, 3) == 0) {
+                bit_depth_ = 8;
+                image_format_ = IMG_FMT_420;
+            } else if (strncmp("422", line, 3) == 0) {
+                bit_depth_ = 8;
+                image_format_ = IMG_FMT_422;
+            } else if (strncmp("444", line, 3) == 0) {
+                bit_depth_ = 8;
+                image_format_ = IMG_FMT_444;
+            } else if (strncmp("420p10", line, 6) == 0) {
+                bit_depth_ = 10;
+                image_format_ = IMG_FMT_420P10_PACKED;
+            } else if (strncmp("422p10", line, 6) == 0) {
+                bit_depth_ = 10;
+                image_format_ = IMG_FMT_422P10_PACKED;
+            } else if (strncmp("444p10", line, 6) == 0) {
+                bit_depth_ = 10;
+                image_format_ = IMG_FMT_444P10_PACKED;
+            } else {
+                printf("chroma format not supported\n");
+                return EB_ErrorBadParameter;
+            }
             fseek(file_handle_, -1, SEEK_CUR);
-
         } break;
         case 'X':  // Comment
         {
@@ -150,8 +181,6 @@ EbErrorType Y4MVideoSource::parse_file_info() {
 
 uint32_t Y4MVideoSource::read_input_frame() {
     uint64_t readSize = 0;
-    const uint32_t input_padded_width = width_;
-    const uint32_t input_padded_height = height_;
     uint8_t* eb_input_ptr = nullptr;
     uint32_t filled_len = 0;
     char frame_header[6] = {0};
@@ -166,33 +195,111 @@ uint32_t Y4MVideoSource::read_input_frame() {
     // return 0;
 
     // Check frame header
-    if (!((strncmp("FRAME", frame_header, 5) == 0) && frame_header[5] == 0xA))
-        return 0;
-
-    frame_buffer_->yStride = input_padded_width;
-    frame_buffer_->cbStride = input_padded_width >> 1;
-    frame_buffer_->crStride = input_padded_width >> 1;
-
-    readSize = (uint64_t)SIZE_OF_ONE_FRAME_IN_BYTES(input_padded_width,
-                                                    input_padded_height);
-
-    uint64_t lumaReadSize = (uint64_t)input_padded_width * input_padded_height;
-
-    // Read raw data from file
-    eb_input_ptr = frame_buffer_->luma;
-    filled_len += (uint32_t)fread(eb_input_ptr, 1, lumaReadSize, file_handle_);
-
-    eb_input_ptr = frame_buffer_->cb;
-    filled_len +=
-        (uint32_t)fread(eb_input_ptr, 1, lumaReadSize >> 2, file_handle_);
-
-    eb_input_ptr = frame_buffer_->cr;
-    filled_len +=
-        (uint32_t)fread(eb_input_ptr, 1, lumaReadSize >> 2, file_handle_);
-
-    if (readSize != filled_len) {
+    if (!((strncmp("FRAME", frame_header, 5) == 0) && frame_header[5] == 0xA)) {
+        printf("Read frame error\n");
         return 0;
     }
+
+    int chroma_down_size = 2;
+    int width_downsize = 1;
+    int height_downsize = 1;
+
+    switch (image_format_) {
+    case IMG_FMT_420: {
+        chroma_down_size = 2;
+        width_downsize = 1;
+        height_downsize = 1;
+        break;
+    }
+    case IMG_FMT_422: {
+        chroma_down_size = 1;
+        width_downsize = 1;
+        height_downsize = 0;
+        break;
+    }
+    case IMG_FMT_444: {
+        chroma_down_size = 0;
+        width_downsize = 0;
+        height_downsize = 0;
+        break;
+    }
+    default: break;
+    }
+
+    frame_buffer_->yStride = width_with_padding_;
+    frame_buffer_->cbStride = width_with_padding_ >> width_downsize;
+    frame_buffer_->crStride = width_with_padding_ >> width_downsize;
+
+    // Read raw data from file
+
+    const int bottom_padding = height_with_padding_ - height_;
+    const int righ_padding = width_with_padding_ - width_;
+    unsigned int read_len = 0;
+    unsigned int i;
+    // Y
+    eb_input_ptr = frame_buffer_->luma;
+    for (i = 0; i < height_; ++i) {
+        read_len = (uint32_t)fread(eb_input_ptr, 1, width_, file_handle_);
+        if (read_len != width_)
+            return 0;  // read file error.
+
+        memset(eb_input_ptr + width_, 0x14, righ_padding);
+        eb_input_ptr += frame_buffer_->yStride;
+        filled_len += frame_buffer_->yStride;
+    }
+    for (i = 0; i < bottom_padding; ++i) {
+        memset(eb_input_ptr, 0x14, width_with_padding_);
+        eb_input_ptr += frame_buffer_->yStride;
+        filled_len += frame_buffer_->yStride;
+    }
+    // Cb
+    eb_input_ptr = frame_buffer_->cb;
+    for (i = 0; i < (height_ >> height_downsize); ++i) {
+        read_len = (uint32_t)fread(
+            eb_input_ptr, 1, (width_ >> width_downsize), file_handle_);
+        if (read_len != width_ >> width_downsize)
+            return 0;  // read file error.
+
+        memset(eb_input_ptr + (width_ >> width_downsize),
+               0x14,
+               righ_padding >> width_downsize);
+        eb_input_ptr += frame_buffer_->cbStride;
+        filled_len += frame_buffer_->cbStride;
+    }
+    for (i = 0; i < (bottom_padding >> height_downsize); ++i) {
+        memset(eb_input_ptr, 0x14, (width_with_padding_ >> width_downsize));
+        eb_input_ptr += frame_buffer_->cbStride;
+        filled_len += frame_buffer_->cbStride;
+    }
+
+    // Cr
+    eb_input_ptr = frame_buffer_->cr;
+
+    for (i = 0; i < (height_ >> height_downsize); ++i) {
+        read_len = (uint32_t)fread(
+            eb_input_ptr, 1, (width_ >> width_downsize), file_handle_);
+        if (read_len != width_ >> width_downsize)
+            return 0;  // read file error.
+
+        memset(eb_input_ptr + (width_ >> width_downsize),
+               0x14,
+               righ_padding >> width_downsize);
+        eb_input_ptr += frame_buffer_->cbStride;
+        filled_len += frame_buffer_->cbStride;
+    }
+    for (i = 0; i < (bottom_padding >> height_downsize); ++i) {
+        memset(eb_input_ptr, 0x14, (width_with_padding_ >> width_downsize));
+        eb_input_ptr += frame_buffer_->cbStride;
+        filled_len += frame_buffer_->cbStride;
+    }
+
+    //     printf("Target:[%dx%d:%d],Read[%d]\r\n",
+    //            width_with_padding_,
+    //            height_with_padding_,
+    //            (width_with_padding_ * height_with_padding_) +
+    //                2 * ((width_with_padding_ * height_with_padding_) >>
+    //                     chroma_down_size),
+    //            filled_len);
 
     return filled_len;
 }
