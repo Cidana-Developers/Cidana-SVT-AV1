3c3975ca148e908ba63f13f7c31a9c135f62ee7f
Wenyao <wenyao.liu@cidana.com>
2019-04-08 16:00:10 +0800

    Initial checkin for Inverse tx assemble test

---
+++ test/InvTxfm2dAsmTest.cc
@@ -0,0 +1,21 @@
+/*
+ * Copyright(c) 2019 Intel Corporation
+ * SPDX - License - Identifier: BSD - 2 - Clause - Patent
+ */
+
+/******************************************************************************
+ * @file FwdTxfm2dTest.c
+ *
+ * @brief Unit test for forward 2d transform functions:
+ * - Av1TransformTwoD_{4x4, 8x8, 16x16, 32x32, 64x64}
+ * - av1_fwd_txfm2d_{rectangle}
+ *
+ * @author Cidana-Edmond, Cidana-Wenyao
+ *
+ ******************************************************************************/
+#include "gtest/gtest.h"
+
+#include <math.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <new>
