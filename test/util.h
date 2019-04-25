990b5be5be16f176460018098f81ff2098e896a5
Wenyao <wenyao.liu@cidana.com>
2019-04-25 12:00:17 +0800

    Fix occasional crash issue as running unit test on Linux
    1. Memory access in write_buffer_16x16 requires 32 byte alignment,
    however the over-aligned types are implementation-defined,
    and can not be guaranteed on different platform.
    To resolve this issue, manually alignment is added.

--- test/util.h
+++ test/util.h
@@ -35,6 +35,11 @@
 #define max(a, b) ((((a) > (b)) ? (a) : (b)))
 #endif
 
+#define ALIGNED_ADDR(T, alignment, buffer) \
+    (T*)(((uintptr_t)buffer + (alignment - 1)) & ~(alignment - 1))
+
+#define ALIGNMENT (32)
+
 namespace svt_av1_test_tool {
 INLINE int32_t round_shift(int64_t value, int32_t bit) {
     assert(bit >= 1);
