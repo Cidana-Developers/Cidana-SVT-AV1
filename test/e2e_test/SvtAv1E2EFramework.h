23e28130f5095b133148cf9483bb5aa6052db65f
edmond-zhu <edmond.zhu@cidana.com>
2019-04-30 15:01:35 +0800

    Refine E2E test framework with delete SvtAv1E2ETestBase and SvtAv1E2EParamBase

--- test/e2e_test/SvtAv1E2EFramework.h
+++ test/e2e_test/SvtAv1E2EFramework.h
@@ -44,36 +44,10 @@ typedef struct {
         *input_picture_buffer; /**< input buffer of encoder in test */
 } SvtAv1Context;
 
-/** SvtAv1E2ETestBase is a basic class for only impelmention of setup, teardown,
- * init and close with normal setting */
-class SvtAv1E2ETestBase : public ::testing::TestWithParam<TestVideoVector> {
-  public:
-    SvtAv1E2ETestBase();
-    virtual ~SvtAv1E2ETestBase();
-
-  protected:
-    void SetUp() override;
-    void TearDown() override;
-    /** initialization for test */
-    virtual void init_test();
-    /** close for test */
-    virtual void close_test();
-    /** test processing body */
-    virtual void run_encode_process() = 0;
-
-  protected:
-    static VideoSource *prepare_video_src(const TestVideoVector &vector);
-
-  protected:
-    VideoSource *video_src_;  /**< video source context */
-    SvtAv1Context av1enc_ctx_; /**< AV1 encoder context */
-    uint32_t start_pos_;      /**< start position of video frame */
-    uint32_t frames_to_test_; /**< frame count for this test */
-};
-
 /** SvtAv1E2ETestFramework is a class with impelmention of video source control,
  * encoding progress, decoding progress, data collection and data comparision */
-class SvtAv1E2ETestFramework : public SvtAv1E2ETestBase {
+class SvtAv1E2ETestFramework
+    : public ::testing::TestWithParam<TestVideoVector> {
   public:
     typedef struct IvfFile {
         FILE *file;
@@ -95,10 +69,21 @@ class SvtAv1E2ETestFramework : public SvtAv1E2ETestBase {
     virtual ~SvtAv1E2ETestFramework();
 
   protected:
+    void SetUp() override;
+    void TearDown() override;
     /** initialization for test */
-    virtual void init_test() override;
+    virtual void init_test();
+    /** close for test */
+    virtual void close_test();
     /** test processing body */
-    void run_encode_process() override;
+    virtual void run_encode_process();
+
+  protected:
+    static VideoSource *prepare_video_src(const TestVideoVector &vector);
+    /** get reconstruction frame from encoder, it should call after send data
+     * @param is_eos  flag of recon frames is eos
+     * into decoder */
+    virtual void get_recon_frame(bool &is_eos);
 
   private:
     /** write ivf header to output file */
@@ -122,15 +107,13 @@ class SvtAv1E2ETestFramework : public SvtAv1E2ETestBase {
     void check_psnr(const VideoFrame &frame);
 
   protected:
-    /** get reconstruction frame from encoder, it should call after send data
-     * @param is_eos  flag of recon frames is eos
-     * into decoder */
-    virtual void get_recon_frame(bool &is_eos);
-
-  protected:
-    ReconSink *recon_sink_; /**< reconstruction frame collection */
-    RefDecoder *refer_dec_; /**< reference decoder context */
-    IvfFile *output_file_;  /**< file handle for save encoder output data */
+    VideoSource *video_src_;   /**< video source context */
+    SvtAv1Context av1enc_ctx_; /**< AV1 encoder context */
+    uint32_t start_pos_;       /**< start position of video frame */
+    uint32_t frames_to_test_;  /**< frame count for this test */
+    ReconSink *recon_sink_;    /**< reconstruction frame collection */
+    RefDecoder *refer_dec_;    /**< reference decoder context */
+    IvfFile *output_file_;     /**< file handle for save encoder output data */
     uint8_t obu_frame_header_size_; /**< size of obu frame header */
     PerformanceCollect *collect_;   /**< performance and time collection*/
     VideoSource *psnr_src_;         /**< video source context for psnr */
