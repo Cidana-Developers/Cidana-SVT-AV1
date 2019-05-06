e18c215b180729f58f28e3227f8d1ea2a9c896d2
Wenyao <wenyao.liu@cidana.com>
2019-05-06 12:51:05 +0800

    Refine the adaptive scan test
    1. refine naming to improve the readability
    2. add comments

--- test/AdaptiveScanTest.cc
+++ test/AdaptiveScanTest.cc
@@ -25,6 +25,8 @@
 #include "EbTransforms.h"
 #include "TxfmCommon.h"
 #include "EbCabacContextModel.h"  // use tx_type_to_class
+#include "util.h"
+
 static bool valid_scan(const int16_t *scan, const int16_t *iscan, int si,
                        int expected_pos) {
     if (scan[si] != expected_pos || iscan[expected_pos] != si) {
@@ -33,7 +35,24 @@ static bool valid_scan(const int16_t *scan, const int16_t *iscan, int si,
         return true;
 }
 
-TEST(AdaptiveScanTest, table_test) {
+/**
+ * @brief Unit test for scan tables:
+ *
+ * Test strategy:
+ * Iterate the positions according to the scan type and verify the pos from
+ * scan table and si from iscan table.
+ *
+ * Expect result:
+ * Position from the scan table should be identical with the pos calculated
+ * from x and y.
+ *
+ * Test coverage:
+ * Test cases:
+ * All valid scan, including zig-zag scan, vertical scan, horizontal scan
+ * and rectangle scan
+ *
+ */
+TEST(AdaptiveScanTest, scan_tables_test) {
     for (int tx_size = TX_4X4; tx_size < TX_SIZES_ALL; ++tx_size) {
         const int txb_height = get_txb_high((TxSize)tx_size);
         const int txb_width = get_txb_wide((TxSize)tx_size);
@@ -47,95 +66,71 @@ TEST(AdaptiveScanTest, table_test) {
             const SCAN_ORDER *scan_order = &av1_scan_orders[tx_size][tx_type];
             const int16_t *scan = scan_order->scan;
             const int16_t *iscan = scan_order->iscan;
+            const int sum_xy = txb_width + txb_height - 1;
+            int si = 0;
             if (tx_class == TX_CLASS_VERT) {
                 // vertical transform will scan the rows horizontally,
                 // so si is the same with pos
                 // process horizontal scan first
-                int si = 0;
-                for (int row = 0; row < txb_height; ++row) {
-                    for (int col = 0; col < txb_width; ++col) {
-                        // convert to block pos and increase it
-                        const int pos = scan[si];
-                        ASSERT_EQ(pos, si)
+                for (int y = 0; y < txb_height; ++y) {
+                    for (int x = 0; x < txb_width; ++x) {
+                        EXPECT_TRUE(valid_scan(scan, iscan, si, x + (y << bwl)))
                             << "tx_size " << tx_size << " tx_type " << tx_type
-                            << " scan failed";
-                        ASSERT_EQ(iscan[pos], si)
-                            << "tx_size " << tx_size << " tx_type " << tx_type
-                            << " iscan failed";
+                            << " scan table test fail";
                         ++si;
                     }
                 }
             } else if (tx_class == TX_CLASS_HORIZ) {
                 // horizontal transform will scan vertically.
-                int si = 0;
-                for (int col = 0; col < txb_width; ++col) {
-                    for (int row = 0; row < txb_height; ++row) {
-                        const int pos = scan[si];
-                        ASSERT_EQ(pos, col + (row << bwl))
+                for (int x = 0; x < txb_width; ++x) {
+                    for (int y = 0; y < txb_height; ++y) {
+                        EXPECT_TRUE(valid_scan(scan, iscan, si, x + (y << bwl)))
                             << "tx_size " << tx_size << " tx_type " << tx_type
-                            << " scan failed";
-                        ASSERT_EQ(iscan[pos], si)
-                            << "tx_size " << tx_size << " tx_type " << tx_type
-                            << " iscan failed";
+                            << " scan table test fail";
                         ++si;
                     }
                 }
             } else if (txb_width < txb_height) {
                 // cols are smaller than rows
-                const int dim = txb_width + txb_height - 1;
-                int si = 0;
-                for (int i = 0; i < dim; ++i) {
-                    for (int row = 0; row < txb_height; ++row) {
-                        int col = i - row;
-                        if (col >= 0 && col < txb_width) {
-                            const int pos = scan[si];
-                            ASSERT_EQ(pos, col + (row << bwl))
+                for (int i = 0; i < sum_xy; ++i) {
+                    for (int y = 0; y < txb_height; ++y) {
+                        int x = i - y;
+                        if (x >= 0 && x < txb_width) {
+                            EXPECT_TRUE(
+                                valid_scan(scan, iscan, si, x + (y << bwl)))
                                 << "tx_size " << tx_size << " tx_type "
-                                << tx_type << " scan failed";
-                            ASSERT_EQ(iscan[pos], si)
-                                << "tx_size " << tx_size << " tx_type "
-                                << tx_type << " iscan failed";
+                                << tx_type << " scan table test fail";
                             ++si;
                         }
                     }
                 }
             } else if (txb_width > txb_height) {
                 // col diag
-                const int dim = txb_width + txb_height - 1;
-                int si = 0;
-                for (int i = 0; i < dim; ++i) {
-                    for (int col = 0; col < txb_width; ++col) {
-                        int row = i - col;
-                        if (row >= 0 && row < txb_height) {
-                            const int pos = scan[si];
-                            ASSERT_EQ(pos, col + (row << bwl))
+                for (int i = 0; i < sum_xy; ++i) {
+                    for (int x = 0; x < txb_width; ++x) {
+                        int y = i - x;
+                        if (y >= 0 && y < txb_height) {
+                            EXPECT_TRUE(
+                                valid_scan(scan, iscan, si, x + (y << bwl)))
                                 << "tx_size " << tx_size << " tx_type "
-                                << tx_type << " scan failed";
-                            ASSERT_EQ(iscan[pos], si)
-                                << "tx_size " << tx_size << " tx_type "
-                                << tx_type << " iscan failed";
+                                << tx_type << " scan table test fail";
                             ++si;
                         }
                     }
                 }
             } else {
                 // zig-zag scan
-                const int dim = txb_width + txb_height - 1;
                 int swap = 1;
-                int si = 0;
-                for (int i = 0; i < dim; ++i) {
+                for (int i = 0; i < sum_xy; ++i) {
                     swap ^= 1;
                     for (int j = 0; j < txb_width; ++j) {
-                        const int col = swap ? i - j : j;
-                        const int row = swap ? j : i - j;
+                        const int x = swap ? i - j : j;
+                        const int y = swap ? j : i - j;
                         if (i - j >= 0 && i - j < txb_width) {
-                            const int pos = scan[si];
-                            ASSERT_EQ(col + (row << bwl), pos)
+                            EXPECT_TRUE(
+                                valid_scan(scan, iscan, si, x + (y << bwl)))
                                 << "tx_size " << tx_size << " tx_type "
-                                << tx_type << " scan failed";
-                            ASSERT_EQ(si, iscan[pos])
-                                << "tx_size " << tx_size << " tx_type "
-                                << tx_type << " iscan failed";
+                                << tx_type << " scan table test fail";
                             ++si;
                         }
                     }
